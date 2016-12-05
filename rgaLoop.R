e8d6c19762fff1c3fdd15abe61f581553779f5e2
Chip Oglesby <chip@creativesparkcolumbia.com>
2016-12-05 10:16:15 -0700

    Updating code

--- rgaLoop.R
+++ rgaLoop.R
@@ -22,3 +22,36 @@ for (i in 1:length(profile$id)){
   return(xy)
 }
 data <- loopData()
+
+profile <- list_profiles()
+
+# Doesn't work
+# Here I've pulled the transaction ID's needed from the previous loop
+# Now I want to get other transactions from users with a given transaction ID.
+# The RGA package doesn't like that the segment does not contain quotations and
+# will return an error.
+
+loopData <- function(){
+for(i in 1:length(data$transaction.Id)){
+  if (i == 1){
+    xy <- get_ga(profile.id=0123456789, start.date = "2015-01-01",
+    end.date = "2015-03-30",
+    dimensions = "ga:transactionId,ga:productName,ga:date,ga:daysToTransaction",
+    metrics = "ga:itemRevenue",
+# Something is wrong with the line below.
+# Should equal to be users::condition::ga:transactionId==0123456
+    segment= users::condition::ga:transactionId=data$transaction.id[i])
+    } else {
+    xz <- get_ga(profile.id=0123456789, start.date = "2015-01-01",
+    end.date = "2015-03-30",
+    dimensions = "ga:transactionId,ga:productName,ga:date,ga:daysToTransaction",
+    metrics = "ga:itemRevenue",
+# Something is wrong with the line below.
+# Should equal to be users::condition::ga:transactionId==0123456
+    segment= users::condition::ga:transactionId=data$transaction.id[i])
+    xy <- rbind(xy,xz)
+  }
+}
+return(xy)
+}
+data <- loopData()
